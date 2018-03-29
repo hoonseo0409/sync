f91d3eaed0868daeb2a9bcc3a0431539819942f9
서훈 <seohoon0409@gmail.com>
2018-03-30 01:15:10 +0900

    지속 감시 및 탐지 제거 성공

--- py2713_64/wooproject.py
+++ py2713_64/wooproject.py
@@ -21,20 +21,40 @@ for line in rdr:
 print coinlist
 f.close()
 
+find_dict={}
+alarm=0
+tick=1
+pygame.mixer.init()
+bang=pygame.mixer.Sound("Alarm05.wav")
 
 while(1):
 
     for coin in coinlist:
+        tick=tick+1
+
+        if alarm==1 and tick%10==0:
+            bang.play()
+            for name, thattime in find_dict.items():
+                print ('{} 의 상장을 탐지한지 {}초 경과되었습니다.'.format(name, time.time()-thattime))
+
         resp=requests.get(coin)
 
         resp.raise_for_status()
         resp.encoding = 'euc-kr'
         html = resp.text
-        #bs = bs4.BeautifulSoup(html, 'html.parser')
         dictio=json.loads(html)
 
-        print dictio['success']
-        
+        #print dictio['success']
+        print (len(coinlist))
+
+        if dictio['success']==False:
+            alarm=1
+            parsed = urlparse.urlparse(coin)
+            find_dict[urlparse.parse_qs(parsed.query)['market'][0][4:]]=time.time()
+            coinlist.remove(coin)
+
+
+        """
         if dictio['success']==False:
             start=time.time()
             while(1):
@@ -44,3 +64,4 @@ while(1):
                 print ('%s 가 상장된 것을 감지한지 %d초가 경과하였습니다.')%(urlparse.parse_qs(parsed.query)['market'][0][4:],(time.time()-start))
                 bang.play()
                 time.sleep(2)
+        """
