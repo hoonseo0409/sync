f772688d29b9523d684602a26add1aa64a9362d7
yerbn0409 <yerbn0409@gmail.com>
2018-03-29 18:29:48 +0900

    url 파싱 성공

--- py2713_64/wooproject.py
+++ py2713_64/wooproject.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 
-import requests, bs4, json, time, csv, io
+import requests, bs4, json, time, csv, io, time, urlparse
 
 import winsound
+
+from flask import request
+
 frequency = 500  # Set Frequency To 2500 Hertz
 duration = 500  # Set Duration To 1000 ms == 1 second
+
+
 
 f = io.open('coinlist.csv', 'r', encoding='utf-8-sig')
 rdr = csv.reader(f)
@@ -18,16 +23,20 @@ f.close()
 while(1):
     i=0
     for coin in coinlist:
-        #resp = requests.get('https://bittrex.com/api/v1.1/public/getticker?market=BTC-ADA')
         resp=requests.get(coin)
+        #test = self.request.get('market', default = '*', type=str)
+        parsed = urlparse.urlparse(coin)
+        print urlparse.parse_qs(parsed.query)['market'][0]
         resp.raise_for_status()
         resp.encoding = 'euc-kr'
         html = resp.text
         #bs = bs4.BeautifulSoup(html, 'html.parser')
         dictio=json.loads(html)
-        print i
-        i=i+1
         print dictio['success']
         if dictio['success']==True:
-            winsound.Beep(frequency, duration)
+            start=time.time()
+            while(1):
+                winsound.Beep(frequency, duration)
+                print('%d초 시간이 지났습니다.')%(time.time()-start)
+
 
