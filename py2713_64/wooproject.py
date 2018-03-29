de001fee7ca09638d1bf711de35cd7fcfd7cd78d
서훈 <seohoon0409@gmail.com>
2018-03-29 12:04:17 +0900

    try dict and str error

--- py2713_64/wooproject.py
+++ py2713_64/wooproject.py
@@ -12,7 +12,8 @@ html = resp.text
 bs = bs4.BeautifulSoup(html, 'html.parser')
 tags = bs.select('success')  # Top 뉴스
 #title = tags.getText()
-print(html)
+print ("HTML%s\n")%html
 #dict= ast.literal_eval(html)
-dict=json.loads(html)
-print dict
+#dict=json.loads(html)
+response_item = ast.literal_eval(json.dumps(html, ensure_ascii=False).encode('utf8'))
+print type(response_item)
