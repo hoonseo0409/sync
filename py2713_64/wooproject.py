90c062bbd51bb292698a90d45d1fdb7685696a92
yerbn0409 <yerbn0409@gmail.com>
2018-03-31 19:35:04 +0900

    url 일정 확률로 404 not found 고치는 중

--- py2713_64/wooproject.py
+++ py2713_64/wooproject.py
@@ -2,7 +2,9 @@
 
 import requests, bs4, json, time, csv, io, time, urlparse, pygame
 
-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 #from flask import request
 
@@ -13,12 +15,13 @@ duration = 2000  # Set Duration To 1000 ms == 1 second
 
 
 
-f = io.open('coinlist.csv', 'r', encoding='utf-8-sig')
+f = io.open('upbit_url2.csv', 'r', encoding='utf-8-sig')
 rdr = csv.reader(f)
+
 coinlist=[]
 for line in rdr:
     coinlist.append(line[0])
-print coinlist
+print (coinlist)
 f.close()
 
 find_dict={}
@@ -27,41 +30,67 @@ tick=1
 pygame.mixer.init()
 bang=pygame.mixer.Sound("Alarm05.wav")
 
+test=['UPBIT', 'KRW']
+round=0
+
 while(1):
+    resp = requests.get(coinlist[0])
+    print coinlist[0]
+    print resp.text
+    print '-----------------------------------------------------------------'
+
+
+
+while(1):
+    
+    
+    print len(coinlist)
+
+    print ('{} rounds was done'.format(round))
+    round=round+1
+
 
     for coin in coinlist:
         tick=tick+1
 
-        if alarm==1 and tick%10==0:
+        if alarm==1 and tick%100==0:
             bang.play()
             for name, thattime in find_dict.items():
                 print ('{} 의 상장을 탐지한지 {}초 경과되었습니다.'.format(name, time.time()-thattime))
 
         resp=requests.get(coin)
 
-        resp.raise_for_status()
-        resp.encoding = 'euc-kr'
+        #resp.raise_for_status()
+        #resp.encoding = 'euc-kr'
+        resp.encoding = 'utf-8-sig'
+
         html = resp.text
-        dictio=json.loads(html)
+
+
+        print coin
+        print html
+
+        print '----------------------------------------------------------'
+
+        for elem in test:
+            if elem in html:
+                alarm = 1
+                parsed = urlparse.urlparse(coin)
+                find_dict[urlparse.parse_qs(parsed.query)['code'][0][15:]] = time.time()
+                coinlist.remove(coin)
+                break
+
+
+
+        #dictio=json.loads(html)
 
         #print dictio['success']
-        print (len(coinlist))
 
+        """
         if dictio['success']==False:
             alarm=1
             parsed = urlparse.urlparse(coin)
             find_dict[urlparse.parse_qs(parsed.query)['market'][0][4:]]=time.time()
             coinlist.remove(coin)
-
-
         """
-        if dictio['success']==False:
-            start=time.time()
-            while(1):
-                pygame.mixer.init()
-                bang=pygame.mixer.Sound("Alarm05.wav")
-                parsed = urlparse.urlparse(coin)
-                print ('%s 가 상장된 것을 감지한지 %d초가 경과하였습니다.')%(urlparse.parse_qs(parsed.query)['market'][0][4:],(time.time()-start))
-                bang.play()
-                time.sleep(2)
-        """
+
