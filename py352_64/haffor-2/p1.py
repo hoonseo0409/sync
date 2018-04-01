a1b681d9370fa195205860fd95d4e9ae9d21e380
yerbn0409 <yerbn0409@gmail.com>
2018-04-01 18:48:08 +0900

    나름 괜찮은 결과 결과치도 비교해서 출력해주고

--- py352_64/haffor-2/p1.py
+++ py352_64/haffor-2/p1.py
@@ -22,12 +22,21 @@ print (len(Y))
 
 # create model
 model = Sequential()
-model.add(Dense(20, input_dim=3, kernel_initializer='normal', activation='relu'))
 model.add(Dense(10, input_dim=3, kernel_initializer='normal', activation='relu'))
+#model.add(Dense(10, input_dim=3, kernel_initializer='normal', activation='relu'))
 model.add(Dense(1, kernel_initializer='normal'))
 # Compile model
 model.compile(loss='mean_squared_error', optimizer='adam')
-model.fit(x=X,y=Y,epochs=500,batch_size=256,validation_split=0.2)
+model.fit(x=X,y=Y,epochs=20000,batch_size=2000,validation_split=0.2)
+
+
+pred=model.predict(x=X, batch_size=1000)
+i=0
+print (len(pred))
+print (len(Y))
+for i in range(1000):
+    print('{} predicted, but {} is true value'.format(pred[i][0],Y[i]))
+
 
 
 """
