2ef4d51e0b5b22b043278865a771b8404f3267fa
yerbn0409 <yerbn0409@gmail.com>
2018-04-01 17:42:36 +0900

    JB pimi indian 돌아는 가는데 정확도 0퍼

--- py352_64/haffor-2/p1.py
+++ py352_64/haffor-2/p1.py
@@ -1,44 +1,24 @@
-
-
-import numpy
-from pandas import read_csv
+# Create your first MLP in Keras
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.wrappers.scikit_learn import KerasRegressor
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
-
-dataframe = read_csv('P1regression.csv',  header=None)
-dataset = dataframe.values
-# split into input (X) and output (Y) variables
-
-X = dataset[:,0:3]
-print(len(X[0]))
-
-Y = dataset[:,3]
-print (len(Y))
-
-# define wider model
-def wider_model():
-    # create model
-    model = Sequential()
-    model.add(Dense(20, input_dim=3, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(1, kernel_initializer='normal'))
-    # Compile model
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    return model
+import numpy
 # fix random seed for reproducibility
-seed = 7
-numpy.random.seed(seed)
-# evaluate model with standardized dataset
-estimators = []
-estimators.append(('standardize', StandardScaler()))
-estimators.append(('mlp', KerasRegressor(build_fn=wider_model, epochs=100, batch_size=5,
-verbose=0)))
-pipeline = Pipeline(estimators)
-kfold = KFold(n_splits=10, random_state=seed)
-results = cross_val_score(pipeline, X, Y, cv=kfold)
-print (results)
-print("Wider: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+numpy.random.seed(7)
+# load pima indians dataset
+dataset = numpy.loadtxt("P1regression.csv", delimiter=",")
+
+# split into input (X) and output (Y) variables
+X = dataset[:,0:3]
+Y = dataset[:,3]
+# create model
+model = Sequential()
+model.add(Dense(12, input_dim=3, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+# Compile model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# Fit the model
+model.fit(X, Y, epochs=150, batch_size=10)
+# evaluate the model
+scores = model.evaluate(X, Y)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
