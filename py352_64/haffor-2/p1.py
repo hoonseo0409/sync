956490399264783d8f0c3f4e0a2945ff572791fe
yerbn0409 <yerbn0409@gmail.com>
2018-04-01 18:25:52 +0900

    model.fit
    256/800 [========>.....................] - ETA: 0s - loss: 361.9453
    800/800 [==============================] - 0s 6us/step - loss: 5587.0088 - val_loss: 5192.3936
    Epoch 412/500

--- py352_64/haffor-2/p1.py
+++ py352_64/haffor-2/p1.py
@@ -20,25 +20,38 @@ print(len(X[0]))
 Y = dataset[:,3]
 print (len(Y))
 
+# create model
+model = Sequential()
+model.add(Dense(20, input_dim=3, kernel_initializer='normal', activation='relu'))
+model.add(Dense(10, input_dim=3, kernel_initializer='normal', activation='relu'))
+model.add(Dense(1, kernel_initializer='normal'))
+# Compile model
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(x=X,y=Y,epochs=500,batch_size=256,validation_split=0.2)
+
+
+"""
 # define wider model
 def wider_model():
     # create model
     model = Sequential()
-    model.add(Dense(20, input_dim=3, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(20, input_dim=3, kernel_initializer='normal', activation='linear'))
     model.add(Dense(1, kernel_initializer='normal'))
     # Compile model
     model.compile(loss='mean_squared_error', optimizer='adam')
     return model
+
 # fix random seed for reproducibility
 seed = 7
 numpy.random.seed(seed)
 # evaluate model with standardized dataset
 estimators = []
 estimators.append(('standardize', StandardScaler()))
-estimators.append(('mlp', KerasRegressor(build_fn=wider_model, epochs=100, batch_size=5,
+estimators.append(('mlp', KerasRegressor(build_fn=model, epochs=100, batch_size=5,
 verbose=0)))
 pipeline = Pipeline(estimators)
 kfold = KFold(n_splits=10, random_state=seed)
 results = cross_val_score(pipeline, X, Y, cv=kfold)
 print (results)
 print("Wider: %.2f (%.2f) MSE" % (results.mean(), results.std()))
+"""
