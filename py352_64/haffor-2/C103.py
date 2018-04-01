979980d8d9ab3859ed4a5eb51236e1c18918b172
yerbn0409 <yerbn0409@gmail.com>
2018-04-02 01:31:12 +0900

    no message

--- py352_64/haffor-2/C103.py
+++ py352_64/haffor-2/C103.py
@@ -6,16 +6,9 @@ import numpy as np
 f=open("filename.bin","wb")
 mydata=np.random.randint(low=1,high=65535,size=10,dtype='uint16')
 print(mydata)
-mydata.astype('int16').tofile('filename.bin')
+mydata.astype('uint16').tofile('filename.bin')
 
-"""
-myfmt='f'*len(mydata)
-#  You can use 'd' for double and < or > to force endinness
-bin=struct.pack(myfmt,*mydata)
-print(bin)
-f.write(bin)
-f.close()
-"""
+
 
 tmp=np.fromfile('filename.bin', dtype='uint16')
 print (tmp)
