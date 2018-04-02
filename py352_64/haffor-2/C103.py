52ab643f10360e9ce3899453fde2788c3fdc7799
yerbn0409 <yerbn0409@gmail.com>
2018-04-02 23:08:50 +0900

    맥북에서 실험해보자 추가적인 64바이트의 정체

--- py352_64/haffor-2/C103.py
+++ py352_64/haffor-2/C103.py
@@ -3,15 +3,23 @@ import numpy as np
 
 
 #mmap_mode
+#b'\x93NUMPY\x01\x00v\x00{\'descr\': \'<u2\', \'fortran_order\': False, \'shape\': (10,), }
+
+mydata=np.random.randint(low=1,high=65535,size=10,dtype='uint16')
+print(mydata)
 
 f=open("filename.bin","ab")
 #f=file('filename.bin', 'a')
-mydata=np.random.randint(low=1,high=65535,size=10,dtype='uint16')
-print(mydata)
+
 #mydata.astype('uint16').tofile('filename.bin')
 np.save(f, mydata)
+
 f.close()
 
 
-tmp=np.fromfile('filename.bin', dtype='uint16')
-print (tmp)
+#tmp=np.fromfile('filename.bin', dtype='uint16')
+#tmp=open('filename.bin', 'rb')
+
+#tmp = np.memmap('filename.bin', dtype='uint16', mode='r', shape=(11))
+tmp = np.memmap('filename.bin', dtype='uint16', mode='r')
+print (len(tmp))
