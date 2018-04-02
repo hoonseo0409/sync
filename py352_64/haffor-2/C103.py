636804dda05b60005a88ff67bd81094488d3146d
yerbn0409 <yerbn0409@gmail.com>
2018-04-02 19:57:52 +0900

    c103 파일 append모드 성공인듯

--- py352_64/haffor-2/C103.py
+++ py352_64/haffor-2/C103.py
@@ -2,12 +2,15 @@ import struct
 import numpy as np
 
 
+#mmap_mode
 
-f=open("filename.bin","wb")
+f=open("filename.bin","ab")
+#f=file('filename.bin', 'a')
 mydata=np.random.randint(low=1,high=65535,size=10,dtype='uint16')
 print(mydata)
-mydata.astype('uint16').tofile('filename.bin')
-
+#mydata.astype('uint16').tofile('filename.bin')
+np.save(f, mydata)
+f.close()
 
 
 tmp=np.fromfile('filename.bin', dtype='uint16')
