df55263ea8883c3307051df8eff456bdb320c130
yerbn0409 <yerbn0409@gmail.com>
2018-04-03 23:00:10 +0900

    우형 프로젝트 시작

--- py352_64/haffor-2/C103.py
+++ py352_64/haffor-2/C103.py
@@ -1,3 +1,5 @@
+#numpy ndarray를 100000만개씩 생성하고 저장하는 것은 성공하였으나, external sort하는 부분은 시간 부족으로 완성하지 못했습니다. external sort 부분은 https://rosettacode.org/wiki/External_sort 를 참고하였습니다.
+
 import struct
 import numpy as np
 import io
@@ -5,10 +7,9 @@ import io
 
 #mmap_mode
 #b'\x93NUMPY\x01\x00v\x00{\'descr\': \'<u2\', \'fortran_order\': False, \'shape\': (10,), }
-
 def makeandsavenp():
-    mydata=np.random.randint(low=1,high=65535,size=10,dtype='uint16')
-    #print(mydata)
+    mydata=np.random.randint(low=1,high=65535,size=100,dtype='uint16')
+    print(mydata)
 
     f=open("hexDec.bin","ab")
     #f=file('filename.bin', 'a')
@@ -18,22 +19,26 @@ def makeandsavenp():
 
     f.close()
 
-
-#tmp=np.fromfile('filename.bin', dtype='uint16')
-tmp=open('hexDec.bin', 'rb')
-
-#tmp = np.memmap('filename.bin', dtype='uint16', mode='r', shape=(11))
-
 for i in range(10):
     makeandsavenp()
 
+
+#tmp=np.fromfile('filename.bin',  dtype='uint16')
+#tmp=open('hexDec.bin', 'rb')
+
+#tmp = np.memmap('filename.bin', dtype='uint16', mode='rb', shape=(11))
+#tmp = np.memmap('filename.bin', dtype='uint16', mode='rb')
+#print (tmp)
+
+print (np.dtype(np.uint16).itemsize)
+
 #tmp = np.memmap('hexDec.bin', dtype='uint16', mode='r')
-tmp2=np.load(tmp)
-print (tmp2)
+
+#tmp2=np.fromfile(, dtype='uint16')
+#print (tmp2)
 
 
-"""
-def sort_large_file(n: int, source: open, sink: open, file_opener=open) -> None:
+def sort_large_file(n: int, source: open, sink: open) -> None:
     '''
         approach:
             break the source into files of size n
@@ -44,14 +49,18 @@ def sort_large_file(n: int, source: open, sink: open, file_opener=open) -> None:
     # store sorted chunks into files of size n
     mergers = []
     while True:
-        text = list(source.read(n))
+        #text = list(source.read(n))
+        text = list(source)
         if not len(text):
             break;
         text.sort()
-        merge_me = file_opener()
-        merge_me.write(''.join(text))
+        merge_me = np.memmap('hexDec.bin', dtype='uint16', mode='readwrite', offset=100*np.dtype(np.uint16).itemsize)
+        #merge_me = open("hexDec.bin","ab")
+        #merge_me.write(''.join(text))
+        np.save(merge_me, text)
         mergers.append(merge_me)
         merge_me.seek(0)
+        merge_me.close()
 
     # merge onto sink
     stack_tops = [f.read(1) for f in mergers]
@@ -75,21 +84,23 @@ def main():
     '''
 
     # load test case into a file like object
-    input_file_too_large_for_memory = io.StringIO('678925341')
+    #input_file_too_large_for_memory = io.StringIO('678925341')
+    input_file_too_large_for_memory=np.memmap('hexDec.bin', dtype='uint16', mode='readwrite')
 
     # generate the expected output
-    t = list(input_file_too_large_for_memory.read())
+    #t = list(input_file_too_large_for_memory.read())
+    t = list(input_file_too_large_for_memory)
     t.sort()
-    expect = ''.join(t)
-    print('expect', expect)
+    #expect = ''.join(t)
+    print('expect', t)
 
     # attempt to sort with several memory sizes
-    for memory_size in range(1, 12):
-        input_file_too_large_for_memory.seek(0)
-        output_file_too_large_for_memory = io.StringIO()
-        sort_large_file(memory_size, input_file_too_large_for_memory, output_file_too_large_for_memory, io.StringIO)
+    for memory_size in range(10):
+        #input_file_too_large_for_memory.seek(0)
+        output_file_too_large_for_memory = open('result.bin', 'wb')
+        sort_large_file(memory_size, input_file_too_large_for_memory, output_file_too_large_for_memory)
         output_file_too_large_for_memory.seek(0)
-        assert (output_file_too_large_for_memory.read() == expect)
+        assert (output_file_too_large_for_memory.read() == t)
         print('memory size {} passed'.format(memory_size))
 
 
@@ -97,4 +108,3 @@ if __name__ == '__main__':
     example = main
     example()
 
-"""
