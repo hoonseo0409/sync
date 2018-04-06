52cb03e762a55162e18ac3e75043e9fd618f5788
yerbn0409 <yerbn0409@gmail.com>
2018-04-06 16:10:08 +0900

    xpath로 찾기까지 됨

--- py2713_64/dcscrap.py
+++ py2713_64/dcscrap.py
@@ -1,7 +1,22 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup
+
 import time
+"""
 browser = webdriver.Chrome("C:\chromedriver.exe")
 browser.get("https://bittrex.com/home/markets")
 time.sleep(30.)
 browser.quit()
 exit()
+"""
+
+driver = webdriver.Chrome("C:\chromedriver.exe")
+driver.get("http://bithumb.cafe/notice")
+#html = driver.page_source
+#soup = BeautifulSoup(html, 'html.parser')
+
+p_element = driver.find_element_by_xpath('//*[@id="primary-fullwidth"]/article[1]/h3/a')
+print p_element.text
+
+
+#print driver.page_source
