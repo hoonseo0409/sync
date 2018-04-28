4ce3bc577d6d62e7aebf43052f3592085f351240
yerbn0409 <yerbn0409@gmail.com>
2018-04-29 01:26:44 +0900

    1번문제 성공

--- py352_64/haffor_3/Partitioning_stocks.py
+++ py352_64/haffor_3/Partitioning_stocks.py
@@ -1,15 +1,29 @@
 import numpy as np
 import random
+import csv
+
 
 stocks=np.genfromtxt('54_hfc_20170614_comp.csv', delimiter=',')
 stocks = stocks.transpose()
+# stocks = stocks.tolist()
+#
+# for i in range(2000):
+#     # stocks[i] = stocks[i].tolist()
+#     stocks[i].append(i) #추후에 shuffle시 index구분을 위해
+#
+# stocks = np.asarray(stocks)
+# print (stocks.shape)
+# print (stocks[1][-1])
+
 print (stocks.shape)
+
 
 LR=1/2000.
 
 divided=[]
 for i in range(10):
     divided.append([])
+argDivided=[]
 
 selected=random.sample(range(2000), 10)
 print (selected)
@@ -53,12 +67,6 @@ for i in range(2000):
                 my_corr=tmp_corr[k]
             else:
                 otherAvg+=tmp_corr[k]
-        # print ('otherAvg shape: {}'.format(otherAvg))
-        # print ('otherAvg: {}'.format(otherAvg))
-        # print ('my_corr: {}'.format(my_corr))
-        # print ('result: {}'.format(my_corr+otherAvg/9.))
-        # print ('score: {}'.format(score))
-        # print ('boolean: {}'.format(np.any(score<my_corr+otherAvg/9.)))
         #최대 점수 찾기
         if (setnum not in already) and (maxscore<my_corr-otherAvg/9.):
             maxscore=my_corr-otherAvg/9.
@@ -67,6 +75,7 @@ for i in range(2000):
     # print (arg)
     #score가 가장 컸던 그룹에 추가
     divided[arg].append(stocks[i])
+    argDivided.append([i, arg])
 
     #10개 그룹 중 하나씩에만 들어가게
     already.append(arg)
@@ -99,23 +108,37 @@ print (num_means)
 before = intraGroupCorr/interGroupCorr
 best=before
 
+
+iteration=1
 stopLoss=0
 notEnough=True
 while(notEnough):
+    argStocks=random.sample(range(2000), 2000)
+    print (argStocks)
+    myRange=[]
+    # for i in range(startPoint, 2000):
+    #     myRange.append(i)
+    # for i in range(0, startPoint):
+    #     myRange.append(i)
+    # np.random.shuffle(stocks)   #이렇게 해주지 않으면 stocks의 앞쪽 데이터들에만 민감하게 학습됩니다.
+    print (stocks.shape)
+
     print ('----------------------------------------------------')
     divided = []
     for i in range(10):
         divided.append([])
+    argDivided = []
 
     num_means = []
     for i in range(10):
         num_means.append(0)
-
-    for i in range(2000):
-        if i%10==0:
+    iteration=0
+    for i in argStocks:
+        if iteration%10==0:
             shuffle*=-1
             already=[]
             # print ('--------------------------')
+        iteration+=1
 
         maxscore=-99999999999999999.
         arg=0
@@ -136,12 +159,6 @@ while(notEnough):
                     my_corr=tmp_corr[k] #10개의 평균 집합 중 자신이 선택한 집합의 벡터와의 corr
                 else:
                     otherAvg+=tmp_corr[k]
-            # print ('otherAvg shape: {}'.format(otherAvg))
-            # print ('otherAvg: {}'.format(otherAvg))
-            # print ('my_corr: {}'.format(my_corr))
-            # print ('result: {}'.format(my_corr+otherAvg/9.))
-            # print ('score: {}'.format(score))
-            # print ('boolean: {}'.format(np.any(score<my_corr+otherAvg/9.)))
             #최대 점수 찾기
             if (setnum not in already) and (maxscore<my_corr-otherAvg/9.):  #200개씩 균등하게 들어가야 하니까, 앞에 벡터가 먼저 들어간 집합에는 들어가지 않는다.
                 maxscore=my_corr-otherAvg/9.
@@ -150,6 +167,7 @@ while(notEnough):
         # print (arg)
         # score가 가장 컸던 그룹에 추가
         divided[arg].append(stocks[i])
+        argDivided.append([i, arg])
 
         # 10개 그룹 중 하나씩에만 들어가게
         already.append(arg)
@@ -157,11 +175,12 @@ while(notEnough):
         # 중심점 평균 업데이트
         num_means[arg] += 1
         means[arg] = (means[arg] + stocks[i]) * num_means[arg] / (num_means[arg] + 1)
+        # means[arg] = (means[arg]+LR*stocks[i])/(1+LR)
 
     #결과들 출력
 
     # print(means)
-
+    print (argDivided)
     interGroupCorr = 0
     interGroupCorrArr = np.corrcoef(means)
     for i in range(10):
@@ -186,11 +205,20 @@ while(notEnough):
     else:
         if now>=best:
             best=now
-            best_divided=divided
+            # best_divided=divided
+            best_argDIvided=argDivided
         stopLoss=0
 
-    if stopLoss==4: #4번 연속 결과가 악화되면
-        print (best_divided.shape)
+    if stopLoss==6: # n번 연속 결과가 악화되면
+        # print (best_divided)
+        # with open('result.csv', 'w', newline='') as myfile:
+        #     wr = csv.writer(myfile)
+        #     wr.writerow(best_argDIvided)
+        with open('result.csv', 'w', newline='') as myfile:
+            wr = csv.writer(myfile)
+            for i in range(2000):
+                wr.writerow(best_argDIvided[i])
+
         notEnough=False
 
     before=now
